891da5e9693b6b004e219e93b6fb07c6cf45699f
Georgiy Garenov <g_garenov@outlook.com>
2024-01-01 21:54:36 +0200

    Successfully connected to the database.

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Uspeshno se vurzahme za bazata");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/", (req, res) => {
+  res.send("Hello bro");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
