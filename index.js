cf7fd31deb9b470813bdb9903e5f5d301aa40568
Georgiy Garenov <g_garenov@outlook.com>
2024-01-01 22:02:34 +0200

    Auth routher was created.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+const authRoute = require("./routes/authRoute");
 const mongoose = require("mongoose");
 
 mongoose
@@ -13,9 +14,7 @@ mongoose
     console.log(err);
   });
 
-app.use("/", (req, res) => {
-  res.send("Hello bro");
-});
+app.use("/api/user", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
