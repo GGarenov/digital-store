ab99b3bceb32f90644a113429da43a546999f456
Georgiy Garenov <g_garenov@outlook.com>
2024-01-03 17:32:26 +0200

    Error handler was created

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const authRoute = require("./routes/authRoute");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+dbConnect();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("Uspeshno se vurzahme za bazata");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", authRoute);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
