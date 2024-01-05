87a56534b6bf3a16111762a94d32f1b60b2e662b
Georgiy Garenov <g_garenov@outlook.com>
2024-01-05 20:34:16 +0200

    Route for all users is created and tested in postman

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser, loginUser } = require("../controller/userController");
+const { createUser, loginUser, getAllUsers } = require("../controller/userController");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/all-users", getAllUsers);
 
 module.exports = router;
