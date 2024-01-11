89ebceb1a432834346ca5de95ff460d1308b4231
Georgiy Garenov <g_garenov@outlook.com>
2024-01-11 11:44:00 +0200

    Sorting logic is created.

--- controller/productController.js
+++ controller/productController.js
@@ -54,6 +54,7 @@ const getSingleProduct = asyncHandler(async (req, res) => {
 //Get all products
 const getAllProducts = asyncHandler(async (req, res) => {
   try {
+    //Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
@@ -61,7 +62,19 @@ const getAllProducts = asyncHandler(async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Product.find(JSON.parse(queryStr));
+    let query = Product.find(JSON.parse(queryStr));
+
+    //Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //Limiting fields
+
     const product = await query;
     res.json(product);
   } catch (error) {
