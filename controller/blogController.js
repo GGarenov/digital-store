c4825f98181397d967566e83224f1094a279e0e6
Georgiy Garenov <g_garenov@outlook.com>
2024-01-12 21:01:46 +0200

    Get All Blogs functionality is created and tested

--- controller/blogController.js
+++ controller/blogController.js
@@ -42,4 +42,14 @@ const getBlog = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createBlog, updateBlog, getBlog };
+//Get All Blogs
+const getAllBlogs = asyncHandler(async (req, res) => {
+  try {
+    const getAllBlogs = await Blog.find();
+    res.json(getAllBlogs);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createBlog, updateBlog, getBlog, getAllBlogs };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createBlog, updateBlog, getBlog } = require("../controller/blogController");
+const { createBlog, updateBlog, getBlog, getAllBlogs } = require("../controller/blogController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBlog);
 router.put("/:id", authMiddleware, isAdmin, updateBlog);
 router.get("/:id", getBlog);
+router.get("/", getAllBlogs);
 
 module.exports = router;
