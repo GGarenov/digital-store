aa8001b09221f2f1e8f35ecfd45fff49525e9169
Georgiy Garenov <g_garenov@outlook.com>
2024-01-13 15:18:09 +0200

    Get all coupons function is created.

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createCoupon } = require("../controller/couponController");
+const { createCoupon, getAllCoupons } = require("../controller/couponController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 
 router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
 
 module.exports = router;
