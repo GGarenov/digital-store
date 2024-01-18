75be233db12cb331ec2b380de26bbf3dfb8f43c0
Georgiy Garenov <g_garenov@outlook.com>
2024-01-19 00:03:04 +0200

    Product card has been created but not finished.

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function ProductCard() {
+  return (
+    <div className="col-3">
+      <div className="product-card">
+        <div className="product-image">
+          <img src="images/watch.jpg" alt="product-image" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">Imeto na produkta</h5>
+          <p className="price">$200</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
