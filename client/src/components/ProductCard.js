d127ae8ede1ef08abcbd76d4f4c1da16e71cebbf
Georgiy Garenov <g_garenov@outlook.com>
2024-01-19 00:04:16 +0200

    Error fixed..

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -5,7 +5,7 @@ function ProductCard() {
     <div className="col-3">
       <div className="product-card">
         <div className="product-image">
-          <img src="images/watch.jpg" alt="product-image" />
+          <img src="images/watch.jpg" alt="product" />
         </div>
         <div className="product-details">
           <h6 className="brand">Havels</h6>
