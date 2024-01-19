2790f3f433603400f4cd2dd9c829d9c40fe08d1a
Georgiy Garenov <g_garenov@outlook.com>
2024-01-19 10:09:46 +0200

    Product card basic functionality is ready and it is implemented in the Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -182,6 +182,20 @@ const Home = () => {
         </div>
       </section>
 
+      <section className="featured-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Featured Products</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
+
       <section className="marque-wrapper py-5">
         <div className="container-xxl">
           <div className="row">
@@ -215,22 +229,6 @@ const Home = () => {
                 </Marquee>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="blog-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <h3 className="section-heading">Featured Products</h3>
-            </div>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
           </div>
         </div>
       </section>
