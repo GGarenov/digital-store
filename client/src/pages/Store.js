a367efe9d16b8cf86991acb4b4c548e234094b55
Georgiy Garenov <g_garenov@outlook.com>
2024-01-20 15:09:33 +0200

    React helmet is installed and Meta file is created where we can store our titles.

--- client/src/pages/Store.js
+++ client/src/pages/Store.js
@@ -1,9 +1,11 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const Store = () => {
   return (
     <>
+      <Meta title="Store" />
       <BreadCrumb title="Store" />
     </>
   );
