9f07a1d0d9dd31ab4b913ad3be12aeaf31c001dc
Georgiy Garenov <g_garenov@outlook.com>
2024-01-21 13:02:37 +0200

    Contact page is created - google maps embeded html was added and some of the css is implemented (but its not completed).

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,7 +1,41 @@
 import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Meta title={"Contact"} />
+      <BreadCrumb title="Contact" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2934.7056380691297!2d23.393225074156792!3d42.64639997116771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa86efecd65cf9%3A0x228909e10a694483!2sCapital%20Fort%2C%20Boulevard%20%22Tsarigradsko%20shose%22%2C%201784%207-Mi%20Kilometar%2C%20Sofia!5e0!3m2!1sen!2sbg!4v1705834448077!5m2!1sen!2sbg"
+                width="600"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div>
+                  <h3 className="contact-title">Contact</h3>
+                </div>
+                <div>
+                  <h3 className="contact-title">Get in touch</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
