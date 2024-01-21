83f84831db4529c3d472a2893f2319a379d371ce
Georgiy Garenov <g_garenov@outlook.com>
2024-01-21 11:01:36 +0200

    I changed part of product card and implemented useLocation in order to sort the products if needed.

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,41 +1,79 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function ProductCard() {
+const ProductCard = (props) => {
+  const { grid } = props;
+  let location = useLocation();
+
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link>
-            <img src="images/wish.svg" alt="wishlist" />
-          </Link>
-        </div>
-        <div className="product-image">
-          <img src="images/watch.jpg" className="img-fluid" alt="product" />
-          <img src="images/watch-2.avif" className="img-fluid" alt="product" />
-        </div>
-        <div className="product-details">
-          <h6 className="brand">Havels</h6>
-          <h5 className="product-title">Imeto na produkta</h5>
-          <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />,<p className="price">$200</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
+    <>
+      <div className={`${location.pathname === "/store" ? `col-${grid}` : "col-3"}`}>
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
             <Link>
-              <img src="images/add-cart.svg" alt="cart" />
-            </Link>
-            <Link>
-              <img src="images/prodcompare.svg" alt="compare" />
-            </Link>
-            <Link>
-              <img src="images/view.svg" alt="view" />
+              <img src="images/wish.svg" alt="wishlist" />
             </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img src="images/watch.jpg" className="img-fluid" alt="product" />
+            <img src="images/watch-2.avif" className="img-fluid" alt="product" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">Imeto na produkta</h5>
+            <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />,
+            <p className="price">$200</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/add-cart.svg" alt="cart" />
+              </Link>
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className={`${location.pathname === "/store" ? `col-${grid}` : "col-3"}`}>
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img src="images/watch.jpg" className="img-fluid" alt="product" />
+            <img src="images/watch-2.avif" className="img-fluid" alt="product" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">Imeto na produkta</h5>
+            <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />,
+            <p className="price">$200</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/add-cart.svg" alt="cart" />
+              </Link>
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
-}
+};
 
 export default ProductCard;
