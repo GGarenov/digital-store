d723d0abaa377124fdf0fbab1cc029dd75e3a850
Georgiy Garenov <g_garenov@outlook.com>
2024-01-21 12:46:18 +0200

    Blog page basic implementation is ready.

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom";
 
 function BlogCard() {
   return (
-    <div className="col-3">
-      <div className="blog-card">
-        <div className="card-image">
-          <img src="images/blog-1.jpg" className="img-fluid" alt="blog" />
-        </div>
-        <div className="blog-content">
-          <p className="date">18 Jan, 2024</p>
-          <h5 className="title">Nqkuv text za produkta</h5>
-          <p className="desc">
-            Lorem ipsum poneje ne mi rabotqt nqkoi snipeti shte si go pisha lorem ipsuma samichak bratle kvo da pravq v
-            23:30 vecherta v chetvurtak bate...
-          </p>
-          <Link to="/" className="button">
-            Read More
-          </Link>
-        </div>
+    <div className="blog-card">
+      <div className="card-image">
+        <img src="images/blog-1.jpg" className="img-fluid w-100" alt="blog" />
+      </div>
+      <div className="blog-content">
+        <p className="date">18 Jan, 2024</p>
+        <h5 className="title">Nqkuv text za produkta</h5>
+        <p className="desc">
+          Lorem ipsum poneje ne mi rabotqt nqkoi snipeti shte si go pisha lorem ipsuma samichak bratle kvo da pravq v
+          23:30 vecherta v chetvurtak bate...
+        </p>
+        <Link to="/" className="button">
+          Read More
+        </Link>
       </div>
     </div>
   );
