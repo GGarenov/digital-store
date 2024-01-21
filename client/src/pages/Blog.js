26391ca2761ab6ee116ec8bd6368dce3ce2fc6c7
Georgiy Garenov <g_garenov@outlook.com>
2024-01-21 12:32:59 +0200

    Blog page is created with routes in app and header

---
+++ client/src/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+
+const Blog = () => {
+  return (
+    <>
+      <Meta title={"Blog"} />
+      <BreadCrumb title="Blog" />
+    </>
+  );
+};
+
+export default Blog;
