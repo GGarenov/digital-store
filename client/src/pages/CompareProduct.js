c1750cc1ca4da843544121c352ee9b83c87cdacf
Georgiy Garenov <g_garenov@outlook.com>
2024-01-23 19:06:22 +0200

    Compare product page basics are ready

--- client/src/pages/CompareProduct.js
+++ client/src/pages/CompareProduct.js
@@ -48,6 +48,43 @@ function CompareProduct() {
                 </div>
               </div>
             </div>
+            <div className="col-3">
+              <div className="compare-product-card position-relative">
+                <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                <div className="product-card-image">
+                  <img src="images/watch.jpg" alt="watch" />
+                </div>
+                <div className="compare-product-details">
+                  <h5 className="title">HonorT1 7.0 1GB RAM 8 GB ROM 7 Inch With Wi-Fi+5G Tablet</h5>
+                  <h6 className="price mb-3 mt-3">$100</h6>
+                  <div>
+                    <div className="product-details">
+                      <h5>Brand:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className="product-details">
+                      <h5>Type:</h5>
+                      <p>Watch</p>
+                    </div>
+                    <div className="product-details">
+                      <h5>Availability:</h5>
+                      <p>In stock</p>
+                    </div>
+                    <div className="product-details">
+                      <h5>Color:</h5>
+                      <Color />
+                    </div>
+                    <div className="product-details">
+                      <h5>Size:</h5>
+                      <div className="d-flex gap-10">
+                        <p>S</p>
+                        <p>M</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
