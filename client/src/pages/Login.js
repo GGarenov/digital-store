23cc8674f719ad63fcbe6586ccf2b392b79bb237
Georgiy Garenov <g_garenov@outlook.com>
2024-01-24 22:41:59 +0200

    Some routes and css were added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -21,7 +21,9 @@ function Login() {
                   <input type="password" name="password" className="form-control" placeholder="password" />
                 </div>
                 <div>
-                  <Link to="/forgot-password">Forgot Password?</Link>
+                  <Link to="/forgot-password" className="mt-3 d-flex justify-content-center gap-20 align-items-center">
+                    Forgot Password?
+                  </Link>
                   <div className="mt-3 d-flex justify-content-center gap-20 align-items-center">
                     <button className="button border-0">Login</button>
                     <Link to="/signup" className="button signup">
