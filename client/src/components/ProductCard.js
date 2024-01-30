79f1dbf25fe492473a9108bee991e8813e9d715b
Georgiy Garenov <g_garenov@outlook.com>
2024-01-30 19:29:17 +0200

    Some warnings were fixed

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -18,9 +18,9 @@ const ProductCard = (props) => {
       <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
         <Link to=":id" className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
-            <Link>
+            <button className="border-0 bg-transperant">
               <img src={wish} alt="wishlist" />
-            </Link>
+            </button>
           </div>
           <div className="product-image">
             <img src={watch} className="img-fluid" alt="product" />
@@ -37,15 +37,15 @@ const ProductCard = (props) => {
           </div>
           <div className="action-bar position-absolute">
             <div className="d-flex flex-column gap-15">
-              <Link>
+              <button className="border-0 bg-transperant">
                 <img src={addcart} alt="cart" />
-              </Link>
-              <Link>
+              </button>
+              <button className="border-0 bg-transperant">
                 <img src={productcompare} alt="compare" />
-              </Link>
-              <Link>
+              </button>
+              <button className="border-0 bg-transperant">
                 <img src={view} alt="view" />
-              </Link>
+              </button>
             </div>
           </div>
         </Link>
