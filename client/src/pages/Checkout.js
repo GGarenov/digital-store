830b5d26c5251c14451fbe0c75b0f8cc11ce6b7c
Georgiy Garenov <g_garenov@outlook.com>
2024-01-30 19:06:51 +0200

    Shipping page is created

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
+import watch from "../images/watch.jpg";
 
 const Checkout = () => {
   return (
@@ -14,9 +15,27 @@ const Checkout = () => {
                 <nav style={{ "--bs-breadcrumb-divider": "'>'" }} aria-label="breadcrumb">
                   <ol className="breadcrumb">
                     <li className="breadcrumb-item">
-                      <a href="#">Home</a>
+                      <Link className="text-dark" to="/cart">
+                        Cart
+                      </Link>
                     </li>
-                    &nbsp; /
+
+                    <li className="breadcrumb-item">
+                      <Link className="text-dark" to="#">
+                        Information
+                      </Link>
+                    </li>
+                    <li className="breadcrumb-item">
+                      <Link className="text-dark" to="#">
+                        Shipping
+                      </Link>
+                    </li>
+
+                    <li className="breadcrumb-item">
+                      <Link className="text-dark" to="#">
+                        Payment
+                      </Link>
+                    </li>
                   </ol>
                 </nav>
                 <h4 className="title">Contact Informaction</h4>
@@ -66,6 +85,44 @@ const Checkout = () => {
                     </div>
                   </div>
                 </form>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="border-bottom py-4">
+                <div className="d-flex gap-10 mb-2 align-items-center">
+                  <div className="w-75 d-flex gap-10">
+                    <div className="w-25 position-relative">
+                      <span
+                        style={{ top: "-10px", right: "2px" }}
+                        className="badge bg-secondary text-white rounded-circle p-2 position-absolute"
+                      >
+                        1
+                      </span>
+                      <img className="img-fluid" src={watch} alt="product" />
+                    </div>
+                    <div>
+                      <h5 className="title">Chasovnik</h5>
+                      <p>Paragrafchi za chasovnika</p>
+                    </div>
+                  </div>
+                  <div className="flex-grow-1">
+                    <h5>$100</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="border-bottom py-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <p>Subtotal</p>
+                  <p>$1000</p>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <p className="mb-0">Shipping</p>
+                  <p className="mb-0">$1000</p>
+                </div>
+                <div className="d-flex justify-content-between align-items-center border-bottom py-4">
+                  <h4 className="total">Total</h4>
+                  <h5 className="total-price">$1000</h5>
+                </div>
               </div>
             </div>
           </div>
