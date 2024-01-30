2224c1d4dc69de1a20a99f632a40a49fb4df3206
Georgiy Garenov <g_garenov@outlook.com>
2024-01-30 20:55:24 +0200

    Services function is created in Data.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import BlogCard from "../components/BlogCard";
 import ProductCard from "../components/ProductCard";
 import SpecialProduct from "../components/SpecialProduct";
 import Container from "../components/Container";
+import { services } from "../utils/Data";
 
 const Home = () => {
   return (
@@ -70,46 +71,20 @@ const Home = () => {
       </Container>
 
       <Container class1="home-wrapper-2 py-5">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <div className="services d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center gap-10">
-                  <img src="images/service.png" alt="services" />
-                  <div>
-                    <h6>Free Shipping</h6>
-                    <p className="mb-0">From all orders over $100</p>
+        <div className="row">
+          <div className="col-12">
+            <div className="services d-flex align-items-center justify-content-between">
+              {services.map((i, j) => {
+                return (
+                  <div className="d-flex align-items-center gap-15" key={j}>
+                    <img src={i.image} alt="services" />
+                    <div>
+                      <h6>{i.title}</h6>
+                      <p className="mb-0">{i.tagline}</p>
+                    </div>
                   </div>
-                </div>
-                <div className="d-flex align-items-center gap-10">
-                  <img src="images/service-02.png" alt="services" />
-                  <div>
-                    <h6>Daily Offers</h6>
-                    <p className="mb-0">Save up to 50% off</p>
-                  </div>
-                </div>
-                <div className="d-flex align-items-center gap-10">
-                  <img src="images/service-03.png" alt="services" />
-                  <div>
-                    <h6>Support 24/7</h6>
-                    <p className="mb-0">Shop with an expert</p>
-                  </div>
-                </div>
-                <div className="d-flex align-items-center gap-10">
-                  <img src="images/service-04.png" alt="services" />
-                  <div>
-                    <h6>Affordable prices</h6>
-                    <p className="mb-0">Get Factory Default Price</p>
-                  </div>
-                </div>
-                <div className="d-flex align-items-center gap-10">
-                  <img src="images/service-05.png" alt="services" />
-                  <div>
-                    <h6>Secure Payments</h6>
-                    <p className="mb-0">100% Protected Payment</p>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </div>
         </div>
