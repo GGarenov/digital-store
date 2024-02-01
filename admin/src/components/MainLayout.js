2411245dd883fc3369e42c87ad05115018925897
Georgiy Garenov <g_garenov@outlook.com>
2024-02-01 20:31:43 +0200

    Work on the main layout. Sidebar is edited - Dashboard, Catalog, Orders and other menus are added.

--- admin/src/components/MainLayout.js
+++ admin/src/components/MainLayout.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { AiOutlineDashboard, AiOutlineShoppingCart, AiOutlineUser, AiOutlineBgColors } from "react-icons/ai";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
 import { Layout, Menu, Button, theme } from "antd";
+
 const { Header, Sider, Content } = Layout;
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const navigate = useNavigate();
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
@@ -20,22 +21,107 @@ const MainLayout = () => {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={["1"]}
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key === "signout") {
+            } else {
+              navigate(key);
+            }
+          }}
           items={[
             {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashboard",
             },
             {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Customers",
             },
             {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
+              key: "Catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "brand",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "brand-list",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "category-list",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category List",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "color-list",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color List",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blog",
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blog",
+              children: [
+                {
+                  key: "blog",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <FaClipboardList className="fs-4" />,
+              label: "Enquiries",
             },
           ]}
         />
