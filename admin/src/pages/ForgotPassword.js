17c2f5857514e2f67cf3f4fb8f5884c7fd2116a7
Georgiy Garenov <g_garenov@outlook.com>
2024-02-01 19:49:22 +0200

    Forgot password page is created

--- admin/src/pages/ForgotPassword.js
+++ admin/src/pages/ForgotPassword.js
@@ -1,7 +1,31 @@
 import React from "react";
+import CustomInput from "../components/CustomInput";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center">Forgot Password</h3>
+        <p className="text-center">Enter your your new password!</p>
+        <form>
+          <CustomInput type="password" label="New password" id="password" />
+          <CustomInput type="password" label="Confirm password" id="password" />
+
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ForgotPassword;
