f5af43f14ed10813f23c6e2fcd7bbbfeef765986
Georgiy Garenov <g_garenov@outlook.com>
2024-02-03 14:18:59 +0200

    Couple of pages are created in the admin panel and data is passed in them.

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,6 +7,14 @@ import ForgotPassword from "./pages/ForgotPassword";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Enquiries from "./pages/Enquiries";
+import BlogList from "./pages/BlogList";
+import BlogCatList from "./pages/BlogCatList";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import ColorList from "./pages/ColorList";
+import CategoryList from "./pages/CategoryList";
+import BrandList from "./pages/BrandList";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
@@ -18,6 +26,14 @@ function App() {
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blog-list" element={<BlogList />} />
+          <Route path="blog-category-list" element={<BlogCatList />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="color-list" element={<ColorList />} />
+          <Route path="category-list" element={<CategoryList />} />
+          <Route path="brand-list" element={<BrandList />} />
+          <Route path="product-list" element={<ProductList />} />
         </Route>
       </Routes>
     </Router>
