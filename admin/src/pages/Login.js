e810126dd08f82c9a6d826ab1df8958a6181d305
Georgi <g_garenov@outlook.com>
2024-02-23 22:37:26 +0200

    Configuring the login with redux, formik and yup

--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -1,8 +1,26 @@
 import React from "react";
 import CustomInput from "../components/CustomInput";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  let schema = Yup.object().shape({
+    email: Yup.string()
+      .email("Email should be valid bratle")
+      .required("Email e zaduljitelen"),
+    password: Yup.string().required("Bez parola ne moje"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
@@ -12,20 +30,44 @@ const Login = () => {
       <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
         <h3 className="text-center">Login</h3>
         <p className="text-center">Login to your account to continue.</p>
-        <form>
-          <CustomInput type="text" label="email" id="email" />
-          <CustomInput type="password" label="password" id="password" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          <CustomInput
+            type="text"
+            name="email"
+            label="email"
+            id="email"
+            val={formik.values.email}
+            onChange={formik.handleChange}
+          />
+          <div className="error">
+            {formik.touched.email && formik.errors.email ? (
+              <div>{formik.errors.email}</div>
+            ) : null}
+          </div>
+          <CustomInput
+            type="password"
+            name="password"
+            label="password"
+            id="password"
+            val={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          <div className="error">
+            {formik.touched.password && formik.errors.password ? (
+              <div>{formik.errors.password}</div>
+            ) : null}
+          </div>
           <div className="mb-3 text-end">
             <Link to="forgot-password">Forgot Password</Link>
           </div>
-          <Link
+          <button
             to="/admin"
             className="border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5"
             style={{ background: "#ffd333" }}
             type="submit"
           >
             Login
-          </Link>
+          </button>
         </form>
       </div>
     </div>
