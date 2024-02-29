515c3164daccd68a5a8efb3f9b45f8b9403d39f3
Georgi <g_garenov@outlook.com>
2024-02-29 22:19:12 +0200

    Customers.js is completed

--- admin/src/pages/Customers.js
+++ admin/src/pages/Customers.js
@@ -1,41 +1,45 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Table } from "antd";
-import { getUsers } from "./path-to-your-action"; // replace 'path-to-your-action' with the actual path
+import { getUsers } from "../features/customers/customerSlice"; // replace 'path-to-your-action' with the actual path
 const columns = [
   {
-    title: "No",
+    title: "SNo",
     dataIndex: "key",
   },
   {
     title: "Name",
     dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
   },
   {
-    title: "Product",
-    dataIndex: "product",
+    title: "Email",
+    dataIndex: "email",
   },
   {
-    title: "Status",
-    dataIndex: "status",
+    title: "Mobile",
+    dataIndex: "mobile",
   },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
 
 const Customers = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUsers());
   }, []);
-  const state = useSelector((state) => state.customer.Customers);
+  const customerState = useSelector((state) => state.customer.customers);
+  const data1 = [];
+  for (let i = 0; i < customerState.length; i++) {
+    if (customerState[i].role !== "admin") {
+      data1.push({
+        key: i + 1,
+        name: customerState[i].firstName + " " + customerState[i].lastName,
+        email: customerState[i].email,
+        mobile: customerState[i].mobile,
+      });
+    }
+  }
+
   return (
     <div>
       <h3 className="mb-4 title">Customers</h3>
