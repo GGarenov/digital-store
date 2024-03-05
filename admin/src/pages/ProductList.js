dd3014aef9e513ad10abf841785b37064b117f4b
Georgi <g_garenov@outlook.com>
2024-03-05 19:52:53 +0200

    Links and some icons were added to design the product-list page

--- admin/src/pages/ProductList.js
+++ admin/src/pages/ProductList.js
@@ -2,23 +2,42 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Table } from "antd";
 import { getProducts } from "../features/product/productSlice";
+import { Link } from "react-router-dom";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
 
 const columns = [
   {
-    title: "No",
+    title: "SNo",
     dataIndex: "key",
   },
   {
     title: "Title",
     dataIndex: "title",
+    sorter: (a, b) => a.title.length - b.title.length,
   },
   {
-    title: "Product",
-    dataIndex: "product",
+    title: "Brand",
+    dataIndex: "brand",
+    sorter: (a, b) => a.brand.length - b.brand.length,
   },
   {
-    title: "Status",
-    dataIndex: "status",
+    title: "Category",
+    dataIndex: "category",
+    sorter: (a, b) => a.category.length - b.category.length,
+  },
+  {
+    title: "Color",
+    dataIndex: "color",
+  },
+  {
+    title: "Price",
+    dataIndex: "price",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
   },
 ];
 
@@ -26,16 +45,28 @@ const ProductList = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProducts());
-  }, [dispatch]);
-  const productState = useSelector((state) => state.product.products);
+  }, []);
+  const productState = useSelector((state) => state.product?.products);
   const data1 = [];
   if (productState) {
     for (let i = 0; i < productState.length; i++) {
       data1.push({
         key: i + 1,
         title: productState[i].title,
-        price: `$ ${productState[i].price}`,
-        status: `London, Park Lane no. ${i}`,
+        brand: productState[i].brand,
+        category: productState[i].category,
+        color: productState[i].color,
+        price: `${productState[i].price}`,
+        action: (
+          <>
+            <Link to="/" className=" fs-3 text-danger">
+              <BiEdit />
+            </Link>
+            <Link className="ms-3 fs-3 text-danger" to="/">
+              <AiFillDelete />
+            </Link>
+          </>
+        ),
       });
     }
   }
