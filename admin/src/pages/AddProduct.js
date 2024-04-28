dd7ec5f15acac046565da5b3f736433f00cc0820
Georgi <g_garenov@outlook.com>
2024-04-28 14:01:58 +0300

    Changes in add product page

--- admin/src/pages/AddProduct.js
+++ admin/src/pages/AddProduct.js
@@ -2,31 +2,27 @@ import React, { useState } from "react";
 import CustomInput from "../components/CustomInput";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import { InboxOutlined } from "@ant-design/icons";
-import { message, Upload } from "antd";
-const { Dragger } = Upload;
-
-const props = {
-  name: "file",
-  multiple: true,
-  action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-};
+import { useFormik } from "formik";
+import * as Yup from "yup";
+let schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Email should be valid bratle")
+    .required("Email e zaduljitelen"),
+  password: Yup.string().required("Bez parola ne moje"),
+});
 
 const AddProduct = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(login(values));
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   const [desc, setDesc] = useState();
   const handleDesc = (e) => {
     setDesc(e);
@@ -57,17 +53,11 @@ const AddProduct = () => {
             <option value="">Select Color</option>
           </select>
           <CustomInput type="number" label="Enter Kolichestvo li kvo" />
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from uploading company data or other banned
-              files.
-            </p>
-          </Dragger>
-          <button className="btn btn-success border-0 rounded-3 my-5" type="submit">
+
+          <button
+            className="btn btn-success border-0 rounded-3 my-5"
+            type="submit"
+          >
             Add Product
           </button>
         </form>
