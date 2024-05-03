ef76d6e475dbb43555066953f8538dd6f3aa94e4
Georgi <g_garenov@outlook.com>
2024-05-03 20:14:10 +0300

    Add category logic is implemented

--- admin/src/features/pcategory/pcategoryService.js
+++ admin/src/features/pcategory/pcategoryService.js
@@ -8,8 +8,15 @@ const getProductCategories = async () => {
   return response.data;
 };
 
+const createCategory = async (category) => {
+  const response = await axios.post(`${base_url}category/`, category, config);
+
+  return response.data;
+};
+
 const pcategoryService = {
   getProductCategories,
+  createCategory,
 };
 
 export default pcategoryService;
