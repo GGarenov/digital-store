95f8767a754cd62477b394699f791de5defed8a9
Georgi <g_garenov@outlook.com>
2024-05-04 22:05:41 +0300

    Add coupon logic is implemented successfully

--- admin/src/pages/AddCoupon.js
+++ admin/src/pages/AddCoupon.js
@@ -1,62 +1,136 @@
 import { React, useEffect } from "react";
-
 import CustomInput from "../components/CustomInput";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import { createBrands } from "../features/brand/brandSlice";
-import { resetState } from "../features/bcategory/bcategorySlice";
+import {
+  createCoupon,
+  getACoupon,
+  resetState,
+  updateACoupon,
+} from "../features/coupon/couponSlice";
 
 let schema = yup.object().shape({
-  title: yup.string().required("Brand name is Required"),
+  name: yup.string().required("Coupon Name is Required"),
+  expiry: yup.date().required("Expiry Date is Required"),
+  discount: yup.number().required("Discount Percentage is Required"),
 });
-
 const AddCoupon = () => {
   const dispatch = useDispatch();
-  const newBrand = useSelector((state) => state.brandReducer);
-  const { isSuccess, isError, isLoading, createdBrand } = newBrand;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const getCouponId = location.pathname.split("/")[3];
+  const newCoupon = useSelector((state) => state.couponReducer.coupons);
+
+  const {
+    isSuccess,
+    isError,
+    isLoading,
+    createdCoupon,
+    couponName,
+    couponDiscount,
+    couponExpiry,
+    updatedCoupon,
+  } = newCoupon;
+  const changeDateFormet = (date) => {
+    const newDate = new Date(date).toLocaleDateString();
+    const [month, day, year] = newDate.split("/");
+    return [year, month, day].join("-");
+  };
+
   useEffect(() => {
-    if (isSuccess && createdBrand) {
-      toast.success("Brand Added Successfullly!");
+    if (getCouponId !== undefined) {
+      dispatch(getACoupon(getCouponId));
+    } else {
+      dispatch(resetState());
     }
-    if (isError) {
+  }, [getCouponId]);
+
+  useEffect(() => {
+    if (isSuccess && createdCoupon) {
+      toast.success("Coupon Added Successfullly!");
+    }
+    if (isSuccess && updatedCoupon) {
+      toast.success("Coupon Updated Successfullly!");
+      navigate("/admin/coupon-list");
+    }
+    if (isError && couponName && couponDiscount && couponExpiry) {
       toast.error("Something Went Wrong!");
     }
   }, [isSuccess, isError, isLoading]);
-
   const formik = useFormik({
+    enableReinitialize: true,
     initialValues: {
-      title: "",
+      name: couponName || "",
+      expiry: changeDateFormet(couponExpiry) || "",
+      discount: couponDiscount || "",
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      dispatch(createBrands(values));
-      formik.resetForm();
-      setTimeout(() => {
+      if (getCouponId !== undefined) {
+        const data = { id: getCouponId, couponData: values };
+        dispatch(updateACoupon(data));
         dispatch(resetState());
-      }, 3000);
+      } else {
+        dispatch(createCoupon(values));
+        formik.resetForm();
+        setTimeout(() => {
+          dispatch(resetState);
+        }, 300);
+      }
     },
   });
+
   return (
     <div>
-      <h3 className="mb-4 title">Add Brand</h3>
+      <h3 className="mb-4 title">
+        {getCouponId !== undefined ? "Edit" : "Add"} Coupon
+      </h3>
       <div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
             type="text"
-            name="title"
-            onChange={formik.handleChange("title")}
-            onBlur={formik.handleBlur("title")}
-            val={formik.values.title}
-            label="Enter brand name"
-            id="brand"
+            name="name"
+            onChange={formik.handleChange("name")}
+            onBlur={formik.handleBlur("name")}
+            val={formik.values.name}
+            label="Enter Coupon Name"
+            id="name"
           />
           <div className="error">
-            {formik.touched.title && formik.errors.title}
+            {formik.touched.name && formik.errors.name}
           </div>
-          <button className="btn btn-success border-0 rounded-3" type="submit">
-            Add Brand
+          <CustomInput
+            type="date"
+            name="expiry"
+            onChange={formik.handleChange("expiry")}
+            onBlur={formik.handleBlur("expiry")}
+            val={formik.values.expiry}
+            label="Enter Expiry Data"
+            id="date"
+          />
+          <div className="error">
+            {formik.touched.expiry && formik.errors.expiry}
+          </div>
+          <CustomInput
+            type="number"
+            name="discount"
+            onChange={formik.handleChange("discount")}
+            onBlur={formik.handleBlur("discount")}
+            val={formik.values.discount}
+            label="Enter Discount"
+            id="discount"
+          />
+          <div className="error">
+            {formik.touched.discount && formik.errors.discount}
+          </div>
+          <button
+            className="btn btn-success border-0 rounded-3 my-5"
+            type="submit"
+          >
+            {getCouponId !== undefined ? "Edit" : "Add"} Coupon
           </button>
         </form>
       </div>
