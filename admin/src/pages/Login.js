cd2a17b8f7dab7f2083dc6e1e5fda70d7851ffe3
Georgi <g_garenov@outlook.com>
2024-05-14 18:10:14 +0300

    Login functionality was edited

--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CustomInput from "../components/CustomInput";
 import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../features/auth/authSlice";
-import { useEffect } from "react";
+
+let schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Email should be valid bratle")
+    .required("Email e zaduljitelen"),
+  password: Yup.string().required("Bez parola ne moje"),
+});
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  let schema = Yup.object().shape({
-    email: Yup.string()
-      .email("Email should be valid bratle")
-      .required("Email e zaduljitelen"),
-    password: Yup.string().required("Bez parola ne moje"),
-  });
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -24,19 +24,21 @@ const Login = () => {
     validationSchema: schema,
     onSubmit: (values) => {
       dispatch(login(values));
-      alert(JSON.stringify(values, null, 2));
     },
   });
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
-    (state) => state.auth
-  );
+
+  const authState = useSelector((state) => state.auth);
+
+  const { user, isError, isSuccess, isLoading, message } = authState;
+
   useEffect(() => {
-    if (!user || isSuccess) {
+    if (isSuccess) {
       navigate("admin");
     } else {
-      alert("greshka priqtel");
+      navigate("");
     }
-  }, [user, isLoading, isError, isSuccess, message, navigate]);
+  }, [user, isError, isSuccess, isLoading]);
+
   return (
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
@@ -46,32 +48,33 @@ const Login = () => {
       <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
         <h3 className="text-center">Login</h3>
         <p className="text-center">Login to your account to continue.</p>
+        <div className="error text-center">
+          {message.message === "Rejected" ? "You are not an Admin" : ""}
+        </div>
         <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
             type="text"
-            name="email"
             label="email"
             id="email"
+            name="email"
+            onChange={formik.handleChange("email")}
+            onBlur={formik.handleBlur("email")}
             val={formik.values.email}
-            onChange={formik.handleChange}
           />
-          <div className="error">
-            {formik.touched.email && formik.errors.email ? (
-              <div>{formik.errors.email}</div>
-            ) : null}
+          <div className="error mt-2">
+            {formik.touched.email && formik.errors.email}
           </div>
           <CustomInput
             type="password"
-            name="password"
             label="password"
             id="password"
+            name="password"
+            onChange={formik.handleChange("password")}
+            onBlur={formik.handleBlur("password")}
             val={formik.values.password}
-            onChange={formik.handleChange}
           />
-          <div className="error">
-            {formik.touched.password && formik.errors.password ? (
-              <div>{formik.errors.password}</div>
-            ) : null}
+          <div className="error mt-2">
+            {formik.touched.password && formik.errors.password}
           </div>
           <div className="mb-3 text-end">
             <Link to="forgot-password">Forgot Password</Link>
