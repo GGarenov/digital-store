42c178c17e48009f4ab5cd45d488db261739b7e2
Georgi <g_garenov@outlook.com>
2024-05-20 17:41:11 +0300

    Redux login logic is implemented in the service and the slice

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -13,6 +13,17 @@ export const registerUser = createAsyncThunk(
   }
 );
 
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.login(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    }
+  }
+);
+
 const initialState = {
   user: "",
   isError: false,
@@ -40,6 +51,29 @@ export const authSlice = createSlice({
         }
       })
       .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error.message;
+        if (state.isError === true) {
+          toast.error(action.error);
+        }
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+
+        if (state.isSuccess === true) {
+          localStorage.setItem("token", action.payload.token);
+          toast.info("User is logged in successfully!");
+        }
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
