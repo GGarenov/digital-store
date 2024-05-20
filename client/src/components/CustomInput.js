18a1d8686ef5f57acdda5535a9ebe0a868b47485
Georgi <g_garenov@outlook.com>
2024-05-20 17:03:53 +0300

    Yup and formik are implemented in the custominputs

--- client/src/components/CustomInput.js
+++ client/src/components/CustomInput.js
@@ -1,9 +1,17 @@
 import React from "react";
 const CustomInput = (props) => {
-  const { type, name, placeholder, className } = props;
+  const { type, name, placeholder, className, value, onChange, onBlur } = props;
   return (
     <div className="mt-3">
-      <input type={type} name={name} placeholder={placeholder} className={`form-control ${className}`} />
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        className={`form-control ${className}`}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
     </div>
   );
 };
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,8 +3,32 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const signUpschema = yup.object({
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last Name is required"),
+  email: yup.string().nullable().email("Email should be valid"),
+  mobile: yup.string().required("Mobile number is required"),
+  password: yup.string().required("Password is required"),
+});
 
 const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      mobile: "",
+      password: "",
+    },
+    validationSchema: signUpschema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
       <Meta title={"Sign Up"} />
@@ -14,35 +38,59 @@ const SignUp = () => {
           <div className="col-12">
             <div className="auth-card">
               <h3 className="text-center mb-3">Create Account</h3>
-              <form className="d-flex flex-column gap-15" action="">
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15"
+              >
                 <CustomInput
                   type="text"
                   name="firstName"
                   placeholder="First Name"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange("firstName")}
+                  onBlur={formik.handleBlur("firstName")}
                 />
+                {formik.touched.firstName && formik.errors.firstName}
                 <CustomInput
                   type="text"
                   name="lastName"
                   placeholder="Last Name"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange("lastName")}
+                  onBlur={formik.handleBlur("lastName")}
                 />
+                {formik.touched.lastName && formik.errors.lastName}
                 <CustomInput
                   type="email"
                   name="email"
                   className="form-control"
                   placeholder="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
                 />
+                {formik.touched.email && formik.errors.email}
                 <CustomInput
                   type="tel"
                   name="mobile"
                   className="form-control"
                   placeholder="Mobile number"
+                  value={formik.values.mobile}
+                  onChange={formik.handleChange("mobile")}
+                  onBlur={formik.handleBlur("mobile")}
                 />
+                {formik.touched.mobile && formik.errors.mobile}
                 <CustomInput
                   type="password"
                   name="password"
                   className="form-control"
                   placeholder="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange("password")}
+                  onBlur={formik.handleBlur("password")}
                 />
+                {formik.touched.password && formik.errors.password}
 
                 <div>
                   <div className="mt-3 d-flex justify-content-center gap-20 align-items-center">
