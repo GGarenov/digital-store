f5a1f2f6980bcc5af232e5b85f1ae30208c0c96d
Georgi <g_garenov@outlook.com>
2024-06-27 22:00:49 +0300

    Special products were removed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -266,14 +266,6 @@ const Home = () => {
         </div>
       </Container>
 
-      <Container class1="special-wrapper py-5 home-wrapper-2">
-        <div className="row">
-          <div className="col-12">
-            <h3 className="section-heading">Special Products</h3>
-          </div>
-        </div>
-      </Container>
-
       <Container class1="popular-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
