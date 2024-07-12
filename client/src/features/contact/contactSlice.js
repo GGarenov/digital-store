53d718fb3ae2ce8c7231749f265c7383c97f26de
Georgi <g_garenov@outlook.com>
2024-07-12 21:42:07 +0300

    Contact slice and services were created and the reducer was added in store.js

---
+++ client/src/features/contact/contactSlice.js
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { contactService } from "./contactService";
+
+export const createQuery = createAsyncThunk(
+  "contact/post",
+  async (contactData, thunkAPI) => {
+    try {
+      return await contactService.postQuery(contactData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue({ message: error.message });
+    }
+  }
+);
+
+const contactState = {
+  contact: "",
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const contactSlice = createSlice({
+  name: "contact",
+  initialState: contactState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createQuery.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createQuery.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.contact = action.payload;
+      })
+      .addCase(createQuery.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.message = action.error.message;
+        if (state.isError === true) {
+          toast.error(action.error);
+        }
+      });
+  },
+});
+
+export default contactSlice.reducer;
