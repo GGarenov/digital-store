9b5e6f96b23b30a1f7c4032743026e3271c0b88c
Georgi <g_garenov@outlook.com>
2024-07-13 11:37:22 +0300

    cart is changed in the user controller also

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -330,38 +330,16 @@ const getWishList = asyncHandler(async (req, res) => {
 
 //Cart functionality
 const userCart = asyncHandler(async (req, res) => {
-  const { cart } = req.body;
+  const { productId, color, quantity, price } = req.body;
   const { _id } = req.user;
   validateMongodbId(_id);
   try {
-    let products = [];
-    const user = await User.findById(_id);
-    //check if user already have product in cart
-    const alreadyExistCart = await Cart.findOne({ orderedBy: user._id });
-    if (alreadyExistCart) {
-      alreadyExistCart.remove();
-    }
-    for (let i = 0; i < cart.length; i++) {
-      let object = {};
-      object.product = cart[i]._id;
-      object.count = cart[i].count;
-      object.color = cart[i].color;
-      //get price for creating total
-      const getPrice = await Product.findById(cart[i]._id)
-        .select("price")
-        .exec();
-      object.price = getPrice.price;
-      products.push(object);
-    }
-    let cartTotal = 0;
-    for (let i = 0; i < products.length; i++) {
-      cartTotal = cartTotal + products[i].price * products[i].count;
-    }
-    console.log(products, cartTotal);
     let newCart = await new Cart({
-      products,
-      cartTotal,
-      orderedBy: user._id,
+      userId: _id,
+      productId,
+      color,
+      quantity,
+      price,
     }).save();
     res.json(newCart);
   } catch (error) {
