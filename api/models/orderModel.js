0a7fa8441fc1062531d28c2e3ae8d7e367e33b64
Georgi <g_garenov@outlook.com>
2024-07-19 16:15:08 +0300

    Order model was edited.

--- api/models/orderModel.js
+++ api/models/orderModel.js
@@ -3,25 +3,89 @@ const mongoose = require("mongoose"); // Erase if already required
 // Declare the Schema of the Mongo model
 var orderSchema = new mongoose.Schema(
   {
-    products: [
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    shippingInfo: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      other: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: Number,
+        required: true,
+      },
+    },
+
+    paymentInfo: {
+      razorpayOrderId: {
+        type: String,
+        required: true,
+      },
+      razorpayPaymentId: {
+        type: String,
+        required: true,
+      },
+    },
+    orderItems: [
       {
         product: {
           type: mongoose.Schema.Types.ObjectId,
+          required: true,
           ref: "Product",
         },
-        count: Number,
-        color: String,
+        color: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Color",
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
       },
     ],
-    paymentIntent: {},
+    paidAt: {
+      type: Date,
+      default: Date.now,
+    },
+    totalPrice: {
+      type: String,
+      required: true,
+    },
+    totalPriceAfterDiscount: {
+      type: String,
+      required: true,
+    },
     orderStatus: {
       type: String,
-      default: "Not Processed",
-      enum: ["Not Processed", "Cash On Delivery", "Processing", "Dispatched", "Cancelled", "Delivered"],
-    },
-    orderedBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      default: "Ordered",
     },
   },
   {
