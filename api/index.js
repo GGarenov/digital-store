47d95b1dc68fbd56b58b78fd1ae9c94b39a7cc0b
Georgi <g_garenov@outlook.com>
2024-07-21 17:44:57 +0300

    Backend part is done for stripe

--- api/index.js
+++ api/index.js
@@ -37,6 +37,37 @@ app.use("/api/color", colorRoute);
 app.use("/api/enquiry", enqRoute);
 app.use("/api/upload", uploadRoute);
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-08-01",
+});
+
+app.get("/config", (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "EUR",
+      amount: 1999,
+      automatic_payment_methods: { enabled: true },
+    });
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(400).send({
+      error: {
+        message: e.message,
+      },
+    });
+  }
+});
+
 app.use(notFound);
 app.use(errorHandler);
 
