3202c94b9ed8f02f8816a4b6ae646e739de93fdf
Georgi <g_garenov@outlook.com>
2024-07-29 23:13:23 +0300

    Call the createOrder function when the user checks out. Modify CheckoutPayment.js to include this functionality:

--- client/src/components/CheckoutPayment.js
+++ client/src/components/CheckoutPayment.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "../pages/CheckoutForm";
+import { useDispatch } from "react-redux";
+import { createOrder } from "../features/user/userSlice";
 
 const CheckoutPayment = () => {
   const [stripePromise, setStripePromise] = useState(null);
   const [clientSecret, setClientSecret] = useState("");
+  const dispatch = useDispatch();
 
   useEffect(() => {
     fetch("http://localhost:3000/config").then(async (r) => {
@@ -24,11 +27,38 @@ const CheckoutPayment = () => {
     });
   }, []);
 
+  const handleCheckout = (paymentInfo) => {
+    const orderData = {
+      shippingInfo: {
+        firstName: "John",
+        lastName: "Doe",
+        address: "123 Main St",
+        city: "New York",
+        state: "NY",
+        other: "Apt 4B",
+        pincode: 10001,
+      },
+      orderItems: [
+        {
+          product: "60d21b967db78e1e48e23c6b", // Replace with actual product ID
+          color: "60d21b967db78e1e48e23c6a", // Replace with actual color ID
+          quantity: 1,
+          price: 100,
+        },
+      ],
+      totalPrice: "100",
+      totalPriceAfterDiscount: "90",
+      paymentInfo: paymentInfo,
+    };
+
+    dispatch(createOrder(orderData));
+  };
+
   return (
     clientSecret &&
     stripePromise && (
       <Elements stripe={stripePromise} options={{ clientSecret }}>
-        <CheckoutForm />
+        <CheckoutForm handleCheckout={handleCheckout} />
       </Elements>
     )
   );
