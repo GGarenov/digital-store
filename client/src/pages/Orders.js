b1ab9826e8be2d64397d1a379e1655ea1e85a66b
Georgi <g_garenov@outlook.com>
2024-07-30 17:38:47 +0300

    Some simple table was created

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,7 +1,65 @@
 import React from "react";
+import Container from "../components/Container";
+import BreadCrumb from "../components/BreadCrumb";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <>
+      <BreadCrumb title="My Orders" />
+      <Container class1="cart-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-3">
+                <h5>Order Id</h5>
+              </div>
+              <div className="col-3">
+                <h5>Total Amount</h5>
+              </div>
+              <div className="col-3">
+                <h5>Amount after discount</h5>
+              </div>
+              <div className="col-3">
+                <h5>Status</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 mt-3">
+            <div className="row">
+              <div className="col-3">
+                <p>Order Id</p>
+              </div>
+              <div className="col-3">
+                <p>Total Amount</p>
+              </div>
+              <div className="col-3">
+                <p>Amount after discount</p>
+              </div>
+              <div className="col-3">
+                <p>Status</p>
+              </div>
+              <div className="col-12">
+                <div className="row bg-secondary p-3">
+                  <div className="col-3">
+                    <p>Order Id</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Total Amount</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Amount after discount</p>
+                  </div>
+                  <div className="col-3">
+                    <p>Status</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Orders;
